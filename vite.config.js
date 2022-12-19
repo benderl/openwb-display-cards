55ec09b1e2d20178686ce6ccf6d8792ddebe1b88
Lutz Bender <github@lutz-bender.de>
2022-12-19 07:56:17 +0100

    updated packages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		nodePolyfills()
+	],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -14,5 +19,13 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: "jsdom",
+	},
+	server: {
+		proxy: {
+			"/ws": {
+				target: "ws://localhost:9001",
+				ws: true,
+			},
+		},
 	},
 });
